202490141a0153bbbbdd1aac4332bd2bfefaef32
michaelviole <michaelviole@gmail.com>
2020-02-24 05:00:02 +0000

    getting there

--- ask.rb
+++ ask.rb
@@ -7,7 +7,6 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 ForecastIO.api_key = "677a036af258586108a34184cb91c5e6"
-forecast = ForecastIO.forecast(@lat,@long).to_hash
 
 get "/" do
   view "search"
@@ -19,6 +18,26 @@ get "/news" do
     @lat = lat_long[0]
     @long = lat_long[1]
     @lat_long = "#{@lat}, #{@long}"
+    
+    forecast = ForecastIO.forecast(@lat,@long).to_hash
+    
+    current_temperature = forecast["currently"]["temperature"]
+    @current_temperature = current_temperature
+    
+    conditions = forecast["currently"]["summary"]
+    @current_conditions = conditions
+
+    for day in forecast["daily"]["data"]
+    puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}"
+    end
+    
+    daily_high = day["temperatureHigh"]
+    @daily_high = daily_high
+
+    daily_summary = day["summary"]
+    @daily_summary = daily_summary
+
+
     
 view "news"
 end
