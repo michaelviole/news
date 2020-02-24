cd9a2210e24b2beeb27357ce7ca99a25ef8efd25
michaelviole <michaelviole@gmail.com>
2020-02-24 03:45:51 +0000

    second edits

--- ask.rb
+++ ask.rb
@@ -6,13 +6,18 @@ require "httparty"
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
-# enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "677a036af258586108a34184cb91c5e6"
+
 
 get "/" do
-  # show a view that asks for the location
+  view "search"
 end
 
 get "/news" do
-  # do everything else
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates
+    @lat = lat_long[0]
+    @long = lat_long[1]
+    @lat_long = "#{@lat}, #{@long}"
+view "news"
 end
